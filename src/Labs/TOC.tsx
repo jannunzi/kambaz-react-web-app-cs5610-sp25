ac2467be0da86b39bde0627b4f40b1fb2341a5b1
Jose Annunziato <jannunzi@gmail.com>
2025-02-04 16:02:28 -0500

    ul to Nav

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,33 +1,33 @@
+import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 export default function TOC() {
   return (
-    <ul>
-      <li>
-        <Link to="/Labs" id="wd-a">
-          {" "}
-          Labs{" "}
-        </Link>
-      </li>
-      <li>
-        <Link to="/Labs/Lab1" id="wd-a1">
+    <Nav variant="pills">
+      <Nav.Item>
+        <Nav.Link as={Link} to="/Labs" id="wd-a">
+          Labs
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link active as={Link} to="/Labs/Lab1" id="wd-a1">
           Lab 1
-        </Link>
-      </li>
-      <li>
-        <Link to="/Labs/Lab2" id="wd-a2">
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link as={Link} to="/Labs/Lab2" id="wd-a2">
           Lab 2
-        </Link>
-      </li>
-      <li>
-        <Link to="/Labs/Lab3" id="wd-a3">
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link as={Link} to="/Labs/Lab3" id="wd-a3">
           Lab 3
-        </Link>
-      </li>
-      <li>
-        <Link to="/Kambaz" id="wd-a3">
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link as={Link} to="/Kambaz" id="wd-a3">
           Kambaz
-        </Link>
-      </li>
-    </ul>
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
   );
 }
