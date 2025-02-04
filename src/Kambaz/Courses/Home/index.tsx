bfd725a4493264698b34d2b1459e18c84609db50
Jose Annunziato <jannunzi@gmail.com>
2025-02-04 16:11:14 -0500

    responsive kambaz courses home

--- src/Kambaz/Courses/Home/index.tsx
+++ src/Kambaz/Courses/Home/index.tsx
@@ -2,17 +2,13 @@ import Modules from "../Modules";
 import CourseStatus from "./Status";
 export default function Home() {
   return (
-    <table id="wd-home">
-      <tr>
-        <td valign="top">
-          {" "}
-          <Modules />{" "}
-        </td>
-        <td valign="top">
-          {" "}
-          <CourseStatus />{" "}
-        </td>
-      </tr>
-    </table>
+    <div id="wd-home" className="d-flex">
+      <div className="flex-fill pe-3">
+        <Modules />
+      </div>
+      <div className="d-none d-xl-block">
+        <CourseStatus />
+      </div>
+    </div>
   );
 }
